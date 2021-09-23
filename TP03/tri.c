dca1ac115e7c89b8e59749e04795bbe9e838dcc1
avril hugo <vrh3938a@u1-204-17l.ens.fsi>
2021-09-23 16:39:47 +0200

    Fin du TP

--- TP03/tri.c
+++ TP03/tri.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void
+int
 somme (int * T, int N){
     int res = 0;
     for (int i = 0 ; i<N ; i++){
@@ -15,7 +15,7 @@ main () {
     for (int i = 0 ; i<10 ; i++){
         T1[i]=i ;
     }
-    s=somme ( T1,10);
+    s = somme ( T1,10);
     printf ("la somme des nombres jusqu'à 9 est %d\n", s);
     return 0 ;
 }
